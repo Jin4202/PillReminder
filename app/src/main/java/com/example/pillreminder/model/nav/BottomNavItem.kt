13da5842aa9da152dce6505ed64dfdc259948c03
Jinseok Heo <jinseokheo@Jinseoks-MacBook-Pro.local>
2025-05-12 22:57:23 -0700

    Added Camera Screen

--- app/src/main/java/com/example/pillreminder/model/nav/BottomNavItem.kt
+++ app/src/main/java/com/example/pillreminder/model/nav/BottomNavItem.kt
@@ -1,13 +1,16 @@
 package com.example.pillreminder.model.nav
 
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.List
+import androidx.compose.material.icons.filled.Camera
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.List
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.ui.graphics.vector.ImageVector
 
 sealed class BottomNavItem(val title: String, val icon: ImageVector, val route: String) {
+    object Camera : BottomNavItem("Camera", Icons.Filled.Camera, "camera")
     object Main : BottomNavItem("Main", Icons.Filled.Home, "main")
-    object Pills : BottomNavItem("Pills", Icons.Filled.List, "pills")
+    object Pills : BottomNavItem("Pills", Icons.AutoMirrored.Filled.List, "pills")
     object Profile : BottomNavItem("Profile", Icons.Filled.Person, "profile")
 }
---
+++ app/src/main/java/com/example/pillreminder/screen/CameraScreen.kt
@@ -0,0 +1,9 @@
+package com.example.pillreminder.screen
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun CameraScreen() {
+    Text("Camera Screen")
+}
