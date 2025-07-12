f12edfac0ec0c3e2dd3da2dcf5da7f168f6a4e2e
Jin4202 <jinseokheo.se@gmail.com>
2025-07-12 16:08:29 -0700

    Add Pill in PillsScreen

--- app/src/main/java/com/example/pillreminder/screen/PillsScreen.kt
+++ app/src/main/java/com/example/pillreminder/screen/PillsScreen.kt
@@ -1,5 +1,7 @@
 package com.example.pillreminder.screen
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
@@ -7,6 +9,9 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import com.example.pillreminder.card.AddPillButton
+import com.example.pillreminder.card.AddPillCard
 import com.example.pillreminder.card.EditPillCard
 import com.example.pillreminder.card.PillItem
 import com.example.pillreminder.model.reminder.Reminder
@@ -18,22 +23,34 @@ fun PillsScreen() {
     var reminders = ReminderManager.getInstance().getReminders()
     var showEditCard by remember { mutableStateOf(false) }
     var selectedReminder by remember { mutableStateOf(Reminder("Not Selected", listOf(LocalTime.of(8,0)), emptySet())) }
+    var showAddCard by remember { mutableStateOf(false) }
 
-    LazyColumn {
-        items(reminders) { reminder ->
-            PillItem(reminder,  onClick = {
-                selectedReminder = reminder
-                showEditCard = true
-            })
+    Column (
+        modifier = Modifier.fillMaxSize()
+    ) {
+        LazyColumn {
+            items(reminders) { reminder ->
+                PillItem(reminder, onClick = {
+                    selectedReminder = reminder
+                    showEditCard = true
+                })
+            }
         }
+        AddPillButton(onClick = {
+            showAddCard = true
+        })
     }
-    if (showEditCard) {
-        // Show the PillCard here
-        EditPillCard (
-            reminder = selectedReminder,
-            showCard = showEditCard,
-            onDismiss = { showEditCard = false },
-            onUpdate = {reminders = ReminderManager.getInstance().getReminders()}
-        )
-    }
+
+    EditPillCard (
+        reminder = selectedReminder,
+        showCard = showEditCard,
+        onDismiss = { showEditCard = false },
+        onUpdate = {reminders = ReminderManager.getInstance().getReminders()}
+    )
+    AddPillCard(
+        showCard = showAddCard,
+        onDismiss = { showAddCard = false },
+        onUpdate = {reminders = ReminderManager.getInstance().getReminders()}
+    )
+
 }
