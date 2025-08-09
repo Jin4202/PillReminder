27e455209d9d205c955beac9c8690ca553d44c5f
Jin4202 <jinseokheo.se@gmail.com>
2025-08-08 22:37:30 -0700

    Added Terms and Conditions. *Link need to be updated if AppName changes

--- app/src/main/java/com/example/pillreminder/screen/SignInScreen.kt
+++ app/src/main/java/com/example/pillreminder/screen/SignInScreen.kt
@@ -5,19 +5,30 @@ import android.content.Intent
 import android.widget.Toast
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.CheckboxDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
 import com.example.pillreminder.R
+import com.example.pillreminder.card.TermsWebViewDialog
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
@@ -29,6 +40,9 @@ fun SignInScreen(
     onSignIn: () -> Unit
 ) {
     val context = LocalContext.current
+    var agreed by remember { mutableStateOf(false) }
+    var showTerms by remember { mutableStateOf(false) }
+    val termsAndConditionLink = "https://www.freeprivacypolicy.com/live/7191cd9e-1f4c-497a-a816-3df6700c91b4"
 
     val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
         .requestIdToken(context.getString(R.string.default_web_client_id))
@@ -74,11 +88,38 @@ fun SignInScreen(
             text = "Please sign in to save your reminders in cloud!"
         )
 
-        Button(onClick = {
+        Button(
+            onClick = {
             val signInIntent: Intent = googleSignInClient.signInIntent
             launcher.launch(signInIntent)
-        }) {
+            },
+            enabled = agreed,
+            modifier = Modifier.padding(top = 8.dp)
+        ) {
             Text("Connect Google Account")
         }
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.padding(top = 8.dp)
+        ) {
+            Checkbox(
+                checked = agreed,
+                onCheckedChange = { agreed = it }
+            )
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Text(
+                    text = "Terms and Conditions",
+                    modifier = Modifier.clickable { showTerms = true },
+                    color = Color.Blue,
+                    textDecoration = TextDecoration.Underline
+                )
+            }
+        }
+
+        TermsWebViewDialog(
+            visible = showTerms,
+            url = termsAndConditionLink,
+            onDismiss = { showTerms = false }
+        )
     }
 }
